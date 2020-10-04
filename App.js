7a77f0216681ca6d5e7d1ec22f289f5f6079b361
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2020-10-03 22:48:01 -0300

    one signal

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import 'react-native-gesture-handler'
-import React from 'react'
+import React, { Component } from 'react'
 import { StatusBar } from 'react-native';
 import Routes from './Routes'
 
-const App = () => {
+import OneSignal from 'react-native-onesignal';
+
+const NewApp = () => {
   return (
     <>
       <StatusBar barStyle='light-content' />
       <Routes />
     </>
   )
+}
+
+class App extends Component {
+  constructor(properties) {
+    super(properties)
+    OneSignal.init("37c4127b-1a52-4bb5-9807-10ffed66a44d")
+
+    OneSignal.addEventListener('opened', this.onOpened)
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('opened', this.onOpened)
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body)
+    console.log('openResult: ', openResult)
+    alert('Message: ', openResult.notification.payload.body)
+  }
+
+  render() {
+    return <NewApp />
+  }
 }
 
 export default App
