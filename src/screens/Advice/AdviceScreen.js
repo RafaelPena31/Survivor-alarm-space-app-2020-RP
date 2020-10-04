a240bc42b6f050790acf2a420d2036c43cf9a7d5
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2020-10-04 10:25:31 -0300

    advices

--- src/screens/Advice/AdviceScreen.js
+++ src/screens/Advice/AdviceScreen.js
@@ -1,18 +1,55 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
+import LinearGradient from 'react-native-linear-gradient'
+import colors from '../../Styles/_colors'
+import headerStyle from '../../components/Header/HeaderStyle'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+import styles from './AdviceStyle'
+import { useNavigation } from '@react-navigation/native'
 /* import { useNavigation } from '@react-navigation/native' */
 
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius eget sem ut rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus eu laoreet magna. Aenean vitae eleifend erat. Proin ex purus, ultricies sed malesuada non, gravida ut urna. Integer auctor consequat pretium. Phasellus id blandit lorem. Sed et leo quis diam vulputate lobortis. Duis venenatis commodo aliquet. Nulla at pellentesque nulla.'
+
 const AdviceScreen = () => {
-  /*   const navigation = useNavigation() */
+  const navigation = useNavigation()
 
   return (
     <SafeAreaView>
-      <ScrollView>
-        <Text>Advice page</Text>
+      <ScrollView style={styles.scrollView}>
+        <View style={headerStyle.headerContainer}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon name='reply' size={40} color={'#222'} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.content}>
+          <Text style={styles.titleProfile}>Advices</Text>
+          <View style={styles.divider}></View>
+          <Text style={styles.subheading}>When to use the sleep inducer?</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+          <Text style={styles.subheading}>Melatonin</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+          <Text style={styles.subheading}>Blue pigmentation makes you stay awake</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+          <Text style={styles.subheading}>Recommendations for a better sleep routine</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+          <Text style={styles.subheading}>How to prevent the jet lag effect</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+          <Text style={styles.subheading}>Items that can be used to ease resting</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+          <Text style={styles.subheading}>When to be exposed to light</Text>
+          <Text style={styles.paragraph}>{lorem}</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
 export default AdviceScreen
+
+
+
+/*
+taxa basa
+
+*/
--- src/screens/Advice/AdviceStyle.js
+++ src/screens/Advice/AdviceStyle.js
@@ -1,5 +1,44 @@
 import { StyleSheet } from 'react-native'
+import colors from '../../Styles/_colors'
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  scrollView: {
+    height: '100%',
+  },
+  content: {
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 30,
+  },
+  divider: {
+    borderWidth: 1,
+    borderColor: colors.secondaryDark,
+    width: 370
+  },
+  titleProfile: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: 35,
+    color: colors.complementaryDark,
+    marginBottom: 30
+  },
+  subheading: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: 22,
+    color: colors.complementaryDark,
+    marginBottom: 5,
+    marginTop: 30,
+    textAlign: 'left',
+    width: '100%',
+  },
+  paragraph: {
+    fontFamily: 'Roboto-Bold',
+    fontSize: 16,
+    color: '#555',
+    marginVertical: 10,
+    paddingLeft: 30,
+    textAlign: 'left',
+    width: '100%',
+  },
+})
 
 export default styles
